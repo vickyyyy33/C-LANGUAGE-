651b068ff4f92f9141fc33dca44869fe0de2766d
vickyyyy33 <vivekburathi2@gmail.com>
2025-11-19 19:12:27 +0530

    Add functionality to sum array elements
    Fix array element addition logic and add input prompt.

---
+++ Addingarrayelement.c
@@ -0,0 +1,18 @@
+//Add array elements
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int arr[10];
+ int sum=0,i;
+printf("Enter the array elements\n");
+for(i=0;i<=9;i++)
+{
+scanf("%d\n",&arr[i]);
+sum=sum+arr[i];
+}
+printf("the sum of array elements is:\n");
+getch();
+}
